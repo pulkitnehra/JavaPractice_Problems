c7f9e1e63bfe9b5fe136dbeb8427e485feefcf01
Pulkit Nehra <nehrapulkit005@gmail.com>
2021-05-28 00:28:52 +0530

    First Commit

---
+++ JavaProblems-Arsh/src/ProblemSet1/DiscountCalculation.java
@@ -0,0 +1,13 @@
+package ProblemSet1;
+
+public class DiscountCalculation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+	
+//	creating a method to calcluate
+	static void calculateDiscount() {
+		
+	}
+}
